1028b287413c3116e74a70cbf61dd27675f1676f
IvanChekrenev <ivan.chekrenev@gmail.com>
2022-04-26 08:55:26 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,74 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingridientsList = document.querySelector('#ingredients');
+
+// version1
+
+// const elements = [];
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+  
+//   const IngridientEl = document.createElement('li');
+//   IngridientEl.classList.add('item');
+//   IngridientEl.textContent = ingredients[i];
+
+//   elements.push(IngridientEl);
+// }
+
+// console.log(elements);
+
+// ingridientsList.append(...elements)
+
+// version 2
+
+// const listIngredients = ingredients.map(ingredient => { 
+
+//   const IngridientEl = document.createElement('li');
+//   IngridientEl.classList.add('item');
+//   IngridientEl.textContent = ingredient;
+
+//   console.log(IngridientEl);
+
+//   return IngridientEl;
+// })
+
+// console.log(listIngredients);
+// ingridientsList.append(...listIngredients);
+
+// version 3 function
+
+const makelistIngredients = (ingredient) => { 
+  return ingredient.map(ingredient => { 
+
+  const IngridientEl = document.createElement('li');
+  IngridientEl.classList.add('item');
+  IngridientEl.textContent = ingredient;
+
+  console.log(IngridientEl);
+
+  return IngridientEl;
+})
+}
+
+const listIngredients = makelistIngredients(ingredients)
+console.log(listIngredients);
+ingridientsList.append(...listIngredients);
+
+
+
+
+// version 4
+
+// const itemList = ingredients.map((item) => { 
+//   return `<li class="item">${item}</li>` 
+// })
+// console.log(itemList);
+
+// ingridientsList.insertAdjacentHTML("afterbegin", itemList);
+// console.log(ingridientsList);
+
+
+
+
