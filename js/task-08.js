46bc5b7cd8558b3643d2e94ebeae69b659137acb
IvanChekrenev <ivan.chekrenev@gmail.com>
2022-04-27 17:46:23 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
 
+const formEl = document.querySelector("form.login-form");
+
+formEl.addEventListener('submit',checkSubmitInput);
+
+function checkSubmitInput(event) { 
+    event.preventDefault();
+    // console.dir(event.currentTarget.elements)
+    // console.dir(event.currentTarget.elements.email.value);
+    // console.dir(event.currentTarget.elements.password.value);
+    if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "") { 
+        alert('Заполни поля!!!')
+    };
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+    const formData = { mail, password };
+    console.log(formData);
+    event.currentTarget.reset();
+
+
+
+    // const formDataNew = new FormData(event.currentTarget);
+    // console.log(formDataNew);
+       
+
+}
+
+
