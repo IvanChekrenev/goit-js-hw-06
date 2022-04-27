a8b169312fe27cee9586b0061fbe6a90c08727d7
IvanChekrenev <ivan.chekrenev@gmail.com>
2022-04-27 18:09:39 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const spanEl = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+const bodyColor = document.querySelector('body')
+
+
+buttonEl.addEventListener('click', changeColorHandle);
+
+function changeColorHandle(event) { 
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+
+}
